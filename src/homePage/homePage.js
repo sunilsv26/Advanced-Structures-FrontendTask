5d5ec4f58f1344456589af50ac81ab82de4088b3
Sunil <sunilsv26@gmail.com>
2021-02-10 12:14:12 +0530

    #3 declared states,functions

--- src/homePage/homePage.js
+++ src/homePage/homePage.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./homePage.module.css";
 
 const mainPage = () => {
+  const [listOfCharacters, setListOfCharacters] = useState([]);
+  const [listOfEpisodes,setListOfEpisodes]=useState([]);
+  const[loadBycharacterList,setLoadByCharacterList]=useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage,setPostsPerPage] = useState(4);
+
+  let charactersList;
+
+  const getCharacterList=()=>{
+
+  }
+
+  const getEpisodesList=()=>{
+      
+  }
   const updateList = (ev) => {};
   return (
     <div className={classes.MainPage}>
