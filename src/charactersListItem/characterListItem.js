12511fb749cd15fe1633557da0d2d516b1468e5c
Sunil <sunilsv26@gmail.com>
2021-02-10 12:38:35 +0530

    #7 defined single characterListItem

---
+++ src/charactersListItem/characterListItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './characterListItem.module.css';
+
+const characterListItem=(props)=>{
+    return(
+        <tr>
+        <td>{props.name}</td>
+        <td>{props.birthday}</td>
+        <td>
+          <img src={props.image} alt="img" />
+        </td>
+        <td>
+          {props.occupation.map((el, idx) => {
+            return idx === props.occupation.length - 1 ? el : `${el}, `;
+          })}
+        </td>
+        <td>{props.nickName}</td>
+        <td>
+          {props.appearance.map((el, idx) => {
+            return idx === props.appearance.length - 1 ? el : `${el}, `;
+          })}
+        </td>
+        <td>{props.portrayed}</td>
+        <td>{props.status}</td>
+      </tr>
+    );
+}
+
+export default characterListItem
